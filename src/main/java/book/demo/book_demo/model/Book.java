cf9cd7521c11d05402633a619e7d9714ffd226f6
johnojetunde <johnojetunde63@gmail.com>
2021-07-21 16:02:31 +0300

    fixed the bug

--- src/main/java/book/demo/book_demo/model/Book.java
+++ src/main/java/book/demo/book_demo/model/Book.java
@@ -3,28 +3,26 @@ package book.demo.book_demo.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.*;
 
 @Data
 @Entity
+@Table
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
+    private Long id;
     @NotBlank(message = "Title is required")
     private String title;
     @NotBlank(message = "Author is required")
     private String author;
-    @Positive (message = "year must be positive")
-    @Min (value=1000, message = "invalid year")
-    @Max (value=2021, message = "invalid year")
-    private int year;
+    @Positive(message = "year must be positive")
+    @Min(value = 1000, message = "invalid year")
+    @Max(value = 2021, message = "invalid year")
+    private Integer year;
     @NotNull(message = "Pages are required")
-    private int pages;
+    private Integer pages;
 
 
 }
